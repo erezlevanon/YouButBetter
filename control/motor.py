adef8f47b28dba5f81049336b1872e1f05dbabb5
erezlevanon <erez.levanon@mail.huji.ac.il>
2018-12-12 20:03:47 +0100

    Basic class for motor controll

---
+++ control/motor.py
@@ -0,0 +1,20 @@
+# Control a single motor
+from RpiMotorLib import RpiMotorLib
+
+class Motor:
+
+    __TYPE = "28BYJ"
+    __STEP_TYPE = "half"
+    __WAIT_TIME = .01
+
+    class DIRECTION(Enum):
+        CW = False
+        CCW = True
+
+
+    def __init__(self, name, pin_1, pin_2, pin_3, pin_4):
+        self.__motor = RpiMotorLib.BYJMotor(name, Motor.__TYPE)
+        self.pins = [pin_1, pin_2, pin_3, pin_4]
+
+    def run(self, direction, steps=512):
+        self.__motor.motor_run(self.pins, Motor.__WAIT_TIME, steps, direction, false, Motor.__STEP_TYPE)
