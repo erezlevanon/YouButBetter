c9dc1d56589e1dea1c6dc780a7bf8c49122fab4e
erezlevanon <erez.levanon@mail.huji.ac.il>
2018-12-13 20:28:04 +0100

    add button for first motor for petri

--- testing/urls.py
+++ testing/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'testing'
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('run', views.run, name='run')
 ]
--- testing/views.py
+++ testing/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
 
 from control import controller
 from control import motor
 
 
 def index(request):
-    c = controller.Controller()
-    c.petri_motor.run(motor.Motor.DIRECTION.CW, 128)
     return render(request, 'testing_page.html')
+
+
+@csrf_exempt
+def run(request):
+    if request.method == 'POST':
+        c = controller.Controller()
+        if request.POST['machine'] == 'petri':
+            c.petri_motor.run(motor.Motor.DIRECTION.CW, 128)
+    return HttpResponseRedirect(reverse('testing:index'))
