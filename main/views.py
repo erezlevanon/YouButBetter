5ee7892243972927b5754abafa880803ef002cd7
erezlevanon <erez.levanon@mail.huji.ac.il>
2019-01-08 19:04:32 +0100

    Create new empty view on main app

--- main/views.py
+++ main/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 from control import controller
-from control import motor
 
 
 def index(request):
     c = controller.Controller()
-    return HttpResponse("Main Page!")
+    return render(request, 'main/interface.html')
--- v1/settings.py
+++ v1/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = [
 
 INSTALLED_APPS = [
     'testing.apps.TestingConfig',
+    'main.apps.MainConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
