aae78d943312c49d0e0efede2419f2986e6b13a4
erezlevanon <erez.levanon@mail.huji.ac.il>
2019-01-09 14:45:22 +0100

    Show some details in main view

--- main/views.py
+++ main/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 
 from control import controller
+from .models import Topic
 
 
 def index(request):
     c = controller.Controller()
-    return render(request, 'main/interface.html')
+    topics = Topic.objects
+    context = {
+        'topics': topics.all(),
+    }
+    return render(request, 'main/interface.html', context)
