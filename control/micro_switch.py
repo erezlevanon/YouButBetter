7775085d49c5f42db7575d9f88c90d8fafee2332
erezlevanon <erez.levanon@mail.huji.ac.il>
2019-01-26 22:44:09 +0100

    simplify microswitch class

--- control/micro_switch.py
+++ control/micro_switch.py
@@ -6,20 +6,10 @@ import RPi.GPIO as GPIO
 class Switch:
     def __init__(self, pin_number):
         self.__pin = pin_number
-        self.__status = False
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.__pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        GPIO.remove_event_detect(self.__pin)
-        # GPIO.add_event_detect(self.__pin, GPIO.FALLING, callback=self.change)
 
     def read(self):
         res = not GPIO.input(self.__pin)
         print(res)
-        return res
-
-    def change(self, channel):
-        if channel:
-            if GPIO.input(channel):
-                self.__status = False
-            else:
-                self.__status = True
+        return GPIO.input(self.__pin)
