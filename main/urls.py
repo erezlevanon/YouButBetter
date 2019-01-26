48d8ae505db5e004b9f6a55bb1c150e2dac38541
erezlevanon <erez.levanon@mail.huji.ac.il>
2019-01-26 22:30:30 +0100

    bugfix: read_tube_done redirects to read_tube.

--- main/urls.py
+++ main/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('produce', views.produce, name='produce'),
     path('read_samples', views.read_samples, name='read_samples'),
     path('read_tube', views.read_tube, name='read_tube'),
-    path('read_tube_done', views.read_tube, name='read_tube_done'),
+    path('read_tube_done', views.read_tube_done, name='read_tube_done'),
 ]
