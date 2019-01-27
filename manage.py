dfe6c7def7e3186894788f5434b3e37a5dcadbc7
erezlevanon <erez.levanon@mail.huji.ac.il>
2019-01-27 17:05:46 +0100

    Add GPIO cleanup on exit to django manage.py

--- manage.py
+++ manage.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 import os
 import sys
+from signal import *
+import RPi.GPIO as GPIO
+
+
+def cleanup(signalnum, stack_trace):
+    GPIO.cleanup()
+    sys.exit(0)
+
 
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'v1.settings')
@@ -12,4 +20,8 @@ if __name__ == '__main__':
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
+    for sig in (SIGINT, SIGABRT, SIGTERM):
+        signal(sig, cleanup)
+
     execute_from_command_line(sys.argv)
